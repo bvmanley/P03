66e37c7cdc75785df45e42fba8f3b8b6da496cd8
mnbeaver <93745090+mnbeaver@users.noreply.github.com>
2021-11-04 17:47:20 -0600

    Update Roster.java

--- Roster.java
+++ Roster.java
@@ -33,8 +33,8 @@ public class Roster {
      */
     public Roster() {
 
-        setStudentList(new ArrayList<Student>());    //needs work
         mStudentList = new ArrayList<>();
+        setStudentList(new ArrayList<Student>()); //needs work
     }
 
     /**
